b3fb59bc530af4909c07d82dd98aabdb10979b03
KymeradThomas <thomas.miglianico@gmail.com>
2020-08-27 18:40:38 +0200

    Episode 4
    4 commit à la suite de : tuto unity fr 2D

---
+++ Assets/Scripts/WeakSpot.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class WeakSpot : MonoBehaviour
+{
+    public GameObject objectToDestroy;
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(objectToDestroy);
+        }
+    }
+}
